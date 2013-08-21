2c846429ba68acf903c5d12c47ce2fa5f8f32c54
Michelangelo Chasseur <chasseurmic@gmail.com>
2013-08-21 12:00:41 +0200

    Add rails_12factor for heroku logging

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 APNS::Application.routes.draw do
-  get "home/index"
   root 'home#index'
+  get "home/index"
   get '/register_device/:token', to: 'home#register_device_with_token'
   get '/send_notification/:token', to: 'home#send_notification'
 end
